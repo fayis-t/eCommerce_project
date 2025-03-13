a93ab01fa1bac5215291c9d43395b204ff32ea45
fayispascon <fayispascon@gmail.com>
2025-03-13 10:23:51 +0530

    changes

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ mongoose.connect(uri)
 .then(() => console.log('Database connected successfully'))
 .catch(err => console.error('Database connection error:', err));
 
-
-
-
-
 const express = require('express');
 const app = express();
 const nocache = require("nocache");
